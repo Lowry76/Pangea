5ce43eee6fe21f0818801cc636b32f3b5d38c4d6
tka007 <tka007@.uit.no>
2013-04-23 10:45:51 +0200

    Initial commit

---
+++ src/Sequence.java
@@ -0,0 +1,29 @@
+
+public class Sequence {
+
+	private String id;
+	private String sequence;
+	
+	public Sequence(String id){
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+	
+	
+	
+}
